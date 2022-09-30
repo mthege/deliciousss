77f962420dcf194862a12b646f82995334e27542
Martina Thege <martinathege@gmail.com>
2022-09-30 13:43:31 +0200

    second commit

---
+++ src/pages/Home.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Veggie from '../components/Veggie'
+import Popular from '../components/Popular'
+
+function Home() {
+    return (
+        <div>
+            <div>
+                <Veggie/>
+                <Popular/>
+            </div>
+        </div>
+    )
+}
+
+export default Home
